463c0eae9ed44bd39d782be7c75c74430d0fdd02
werthzack <timmyabiola27@gmail.com>
2025-11-24 14:40:25 +0000

    Iniitialized Monorepo with two server apps

---
+++ apps/server/listen.js
@@ -0,0 +1,10 @@
+const app = require('./app.js');
+const { PORT = 9090 } = process.env;
+
+app.listen(PORT, (err) => {
+  if (err) {
+    console.log('The server has encountered an error: \n', err);
+  } else {
+    console.log('Express Http Server active: Listening on port', PORT);
+  }
+});
