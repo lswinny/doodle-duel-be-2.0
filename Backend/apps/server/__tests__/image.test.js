403d528bd492027c6f1e2c2c3d890f5197f402cf
Lydia <lswinny@gmail.com>
2025-12-06 23:23:18 +0000

    Fix messy folder structure

--- Backend/apps/server/__tests__/image.test.js
+++
@@ -1,6 +0,0 @@
-const request = require('supertest');
-const app = require('../app.js');
-
-describe('Image Sanitization Test', () => {
-  test('Invalidates an image when detected an artificially corrupt file', () => {});
-});
